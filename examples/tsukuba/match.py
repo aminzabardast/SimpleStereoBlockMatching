703be8ee0a8504001646960e5f79845f9922673a
Amin Zabardast <aminzabardast@gmail.com>
2018-04-16 20:41:43 +0300

    Using the algorithm on Tsukuba example

---
+++ examples/tsukuba/match.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from SimpleBM.matching import BlockMatcher
+
+# Importing Images and converting to gray scale
+imgL = plt.imread('images/left.ppm')
+imgL = np.dot(imgL[..., :], [.333, .333, .334])
+imgR = plt.imread('images/right.ppm')
+imgR = np.dot(imgR[..., :], [.333, .333, .334])
+
+# Disparity Template
+disp = np.ndarray(shape=imgL.shape)
+
+# Running the algorithm
+matcher = BlockMatcher(leftImage=imgL, rightImage=imgR)
+matcher.compute()
+
+# Saving the image
+disp[:,:] = matcher.disparity()/15
+plt.imsave('result.png', disp)
