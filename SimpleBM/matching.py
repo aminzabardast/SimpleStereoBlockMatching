5755f79f540ba7cf76bb8c63e02ecf86e0a5fe32
Amin Zabardast <aminzabardast@gmail.com>
2018-04-18 18:48:55 +0300

    Implementing scale space into algorithm

--- SimpleBM/matching.py
+++ SimpleBM/matching.py
@@ -2,43 +2,44 @@ import numpy as np
 
 
 class BlockMatcher:
-    def __init__(self, left_image, right_image, block_size=5, disparity_range=16):
+    def __init__(self, left_image, right_image, block_size=5, disparity_range=16, scale_space=3):
         if left_image.shape != right_image.shape:
             raise IndexError('Left and Right images do not have the same dimensions.')
 
         if block_size < 1 or block_size % 2 == 0:
             raise IndexError('Kernel size is not a positive odd number.')
 
-        self._leftImage = left_image
-        self._rightImage = right_image
-        self._blockSize = block_size
-        self._disparityRange = disparity_range
+        self._left_scale_space = [left_image]
+        self._right_scale_space = [right_image]
+        self._block_size = block_size
+        self._disparity_range = disparity_range
 
         # Initial disparity
-        self._disparity_map = np.zeros(shape=self._leftImage.shape)
+        self._disparity_map = np.zeros(shape=self._left_scale_space[0].shape)
+        self._create_pyramids(scale_space)
 
     def _compute_for_single_channel_image(self):
         """Algorithm for single channel images"""
         # Left Image Dimensions
-        rows, columns = self._leftImage.shape
+        rows, columns = self._left_scale_space[0].shape
 
         # Avoiding Index Errors
-        margin = self._blockSize // 2
+        margin = self._block_size // 2
         # Iterating rows
         for i in range(margin, rows - margin):
             # Iterating Columns
             for j in range(margin, columns - margin):
                 # Extracting left block
-                left_block = self._leftImage[i - margin:i + margin + 1, j - margin:j + margin + 1]
+                left_block = self._left_scale_space[0][i - margin:i + margin + 1, j - margin:j + margin + 1]
                 # Error Array
                 errors = np.ndarray(shape=(0,))
                 # End of search in the row, beginning from j
-                search_end = margin - 1 if j - self._disparityRange < margin - 1 else j - self._disparityRange
+                search_end = margin - 1 if j - self._disparity_range < margin - 1 else j - self._disparity_range
                 # Searching for match in the right image
                 # The match will be in the left side of the current poisson
                 for k in range(j, search_end, -1):
                     # Extracting right block
-                    right_block = self._rightImage[i - margin:i + margin + 1, k - margin:k + margin + 1]
+                    right_block = self._right_scale_space[0][i - margin:i + margin + 1, k - margin:k + margin + 1]
                     # Calculating Error
                     errors = np.append(errors, self._mse(left_block, right_block))
                 # Minimum Index
@@ -53,16 +54,22 @@ class BlockMatcher:
 
     def _mse(self, left_block, right_block):
         """Calculating 'Mean Squared Error"""
-        return np.sum(np.power(left_block - right_block, 2)) / self._blockSize ** 2
+        return np.sum(np.power(left_block - right_block, 2)) / self._block_size ** 2
 
     def _mae(self, left_block, right_block):
         """Calculating 'Mean Absolute Error'"""
-        return np.sum(np.abs(left_block - right_block)) / self._blockSize ** 2
+        return np.sum(np.abs(left_block - right_block)) / self._block_size ** 2
+
+    def _create_pyramids(self, n):
+        for layer in range(1, n):
+            self._left_scale_space.append(self._left_scale_space[layer-1][0::2, 0::2])
+            self._right_scale_space.append(self._right_scale_space[layer-1][0::2, 0::2])
+        return True
 
     def compute(self):
         """Main Simple Block Matching Algorithm / Input images should be rectified"""
         # Gray Scale Images
-        if len(self._leftImage.shape) == 2:
+        if len(self._left_scale_space[0].shape) == 2:
             self._compute_for_single_channel_image()
         # Not Gray Scale
         else:
