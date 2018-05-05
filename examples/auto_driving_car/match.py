f53e98b1c458c930de9ae3156638ff0e9771866f
Amin Zabardast <aminzabardast@gmail.com>
2018-05-05 14:09:03 +0300

    New Example. Field of AutoDriving Cars.

---
+++ examples/auto_driving_car/match.py
@@ -0,0 +1,40 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time
+sys.path.append('../../')
+from SimpleSM.matching import StereoMatcher
+
+# Importing Images and converting to gray scale
+imgL = plt.imread('images/left.png')
+imgL = np.dot(imgL[..., :], [.25, .25, .25, .25])
+imgR = plt.imread('images/right.png')
+imgR = np.dot(imgR[..., :], [.25, .25, .25, .25])
+
+# Dynamic Programming
+# Running the algorithm
+matcher = StereoMatcher(imgL, imgR, method="dynamic_programming")
+
+# Computing main method + run time
+start_time = time()
+matcher.compute()
+end_time = time()
+print('Runtime for dynamic programming is: {} s'.format(end_time-start_time))
+
+# Saving the image
+disparity_map = matcher.get_disparity_image()
+plt.imsave('dp.png', disparity_map, cmap=plt.get_cmap('jet'))
+
+# Block Matching
+# Running the algorithm
+matcher = StereoMatcher(imgL, imgR, method="block_matching", disparity_range=128, var_threshold=1, min_block_size=5, max_block_size=15)
+
+# Computing main method + run time
+start_time = time()
+matcher.compute()
+end_time = time()
+print('Runtime for block matching is: {} s'.format(end_time-start_time))
+
+# Saving the image
+disparity_map = matcher.get_disparity_image()
+plt.imsave('bm.png', disparity_map, cmap=plt.get_cmap('jet'))
